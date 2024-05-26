3f9ebcadd6ac02b115ece38a65dd14240652bd91
днс <kreshvan12317@yandex.ru>
2024-05-26 08:30:42 +0300

    на проверку!!!

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ public class Main {
 
         //задание 1
         int salary = 15000;
-        int total = 0;
-        int month = 0;
+        int total = 0; //общий
+        int month = 0; //месяц
         while (total <= 2459000) { //пока-(While) - делай так(System.out)
             month = month + 1;
             total = salary + total;
             System.out.println("Месяц" + month + "сумма накоплений равна" + total + "рублей");
         }
+
+
+
 //задание 1.1 для себя
         int salary1 = 15000;
         int month1 = 0;
@@ -19,53 +22,72 @@ public class Main {
             month1 = month1 + 1;
             System.out.println("Месяц" + month1 + "сумма накоплений равна" + total1 + "рублей");
         }
+
+
+
         //задание 2
         int number = 1;
-        while (number <= 10) {
+        int finalfigure = 10;// конечная цифра
+        while (number <=finalfigure ) {
             System.out.print(number);
             number = number + 1;
         }
         System.out.println(" ");
-        for (number = 10; number >= 1; number--) {
+        for (number = finalfigure; number >= 1; number--) {
             System.out.print(number);
         }
         System.out.println(" ");
-//задание 3d
+
+
+
+//задание 3
         //peopleYear = рождаемость - смертность = 9 (С 1000чел)
         // за год с 12000000 (9 * 12000)
-        int PeopleYear = 0;
-        int oneYear = 0;
-        int summaPeople = 12000000;
-        int quantityPeopleFromThousand = 9;
-        for (oneYear = 1; oneYear <= 10; oneYear++) {
-            // oneYear = oneYear+1;
-
-            PeopleYear+= ( quantityPeopleFromThousand * summaPeople / 1000) ;
-            System.out.println("Год" + oneYear + "численность населения составляет" + PeopleYear);
+        int peopleYear = 0;
+        int oneYear = 1;
+        int summaPeople = 12000000; //количество населения
+          int birthRate = 17;//смертность
+        int mortality = 8;//рождаемость
+        int yearFinish = 10;// за какое количество лет
+        for (; oneYear <= yearFinish; oneYear++) {
+            peopleYear+= ((birthRate- mortality) * summaPeople / 1000) ; //1000 - нужно ли ее определять в переменную?
+            System.out.println("Год" + oneYear + "численность населения составляет" + peopleYear);
         }
-        //задание 4
-        int summaOne = 15000;
-        int general = 0;
-        int month2 = 0;
-        for(; general<=12000000; ){
 
-            month2 = month2+1;
-            general= general + general/100*7;
-            general = general + summaOne;
+
+        //задание 4
+        int firstDepositAmount = 15000;// первая сумма вклада
+        int general = 0;// общая сумма накоплений(вклад + проценты)
+        int month2 = 0; //месяц
+int finalSumma = 12000000; //конечная сумма накоплений
+int contributionPercentage = 7;//проценты банка от накоплений
+             for(; general<=finalSumma;month2++ ){
+            general= general + general/100*contributionPercentage; // 100% (для вычесления 1%)
+            general = general + firstDepositAmount;
             System.out.println("месяц" + month2+"сумма" +general);
         }
+
+
+
 //задание 5
-        int summaOne1 = 15000;
-        int general1 = 0;
-        int month3 = 0;
-        for( ;general1<=12000000;  ){
+        int firstDepositAmount1 = 15000;// первая сумма вклада
+        int general1 = 0;// общая сумма накоплений(вклад + проценты)
+        int month3 = 0; //месяц
+        int finalSumma1 = 12000000; //конечная сумма накоплений
+        int contributionPercentage1 = 7;//проценты банка от накоплений
+        for( ;general1<=finalSumma1;  ){
 
             month3 = month3+1;
-            general1= general1 + summaOne1/100*7;
-            general1 = general1 + summaOne1;
+            general1= general1 + firstDepositAmount1/100*contributionPercentage1;
+            general1 = general1 + firstDepositAmount1;
             if(month3%6==0)
                 System.out.println("месяц" + month3+"сумма" +general1);
         }
+
+
+
+
+
         //задание 6
         int summaOne2 = 15000;
         int general2 = 0;
@@ -79,7 +101,6 @@ public class Main {
         }
 
 //задание 7
-
         for(int friday = 5; friday<=31;){
 
             friday= friday+7;
